101ff75e4292d701564eb556a84ea1d6cf9c92cc
Vishal Gautam <ivishalgautamm@gmail.com>
2025-02-14 12:44:03 +0530

    added new api in treatments

--- app/api/treatment/routes.js
+++ app/api/treatment/routes.js
@@ -5,9 +5,9 @@ import controller from "./controller.js";
 export default async function routes(fastify, opts) {
   fastify.post("/", {}, controller.create);
   fastify.put("/:id", {}, controller.updateById);
+  fastify.get("/getByPatientId", {}, controller.getByPatientId);
   fastify.get("/:id", {}, controller.getById);
   fastify.get("/getByClinicId/:id", {}, controller.getByClinicId);
-  fastify.get("/getByPatientId", {}, controller.getByPatientId);
   fastify.get(
     "/getByPatientAndClinicId/:patient_id/:clinic_id",
     {},
