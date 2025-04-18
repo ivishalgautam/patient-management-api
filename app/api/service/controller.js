0919752fc5db49b5b5c8fe7336e7d18e61a46d20
Vishal Gautam <ivishalgautamm@gmail.com>
2025-04-18 14:57:18 +0530

    removed services pricing

--- app/api/service/controller.js
+++ app/api/service/controller.js
@@ -120,7 +120,7 @@ const get = async (req, res) => {
 };
 
 const deleteById = async (req, res) => {
-  const transaction = sequelize.transaction();
+  const transaction = await sequelize.transaction();
 
   try {
     const record = await table.ServiceModel.getByPk(req);
