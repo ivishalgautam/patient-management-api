08acf289a77603ec19ae7722354fbd1f640ed449
Vishal Gautam <ivishalgautamm@gmail.com>
2025-05-22 10:49:32 +0530

    bug fix

--- app/api/auth/routes.js
+++ app/api/auth/routes.js
@@ -3,6 +3,12 @@ import controller from "./controller.js";
 import userController from "../users/controller.js";
 
 export default async function routes(fastify, options) {
+  fastify.addHook("preHandler", async (request, reply) => {
+    request.body && console.log("body", request.body);
+  });
+  fastify.addHook("onSend", async (request, reply, payload) => {
+    console.log({ response: payload }, "Response payload");
+  });
   fastify.post("/login", {}, controller.verifyUserCredentials);
   fastify.post("/register", {}, controller.createNewUser);
   fastify.post("/refresh", {}, controller.verifyRefreshToken);
