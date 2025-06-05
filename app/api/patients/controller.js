88147a601fc220905ae11c1d0aefc7bd6943399f
Vishal Gautam <ivishalgautamm@gmail.com>
2025-06-05 17:39:11 +0530

    added import services and users

--- app/api/patients/controller.js
+++ app/api/patients/controller.js
@@ -28,7 +28,7 @@ const importPatients = async (req, res) => {
             String(user.fullname.substring(0, 4)).toLowerCase() +
             user.mobile_number.slice(-4);
           const hashed_password = hash.encrypt(password);
-          console.log({ username, password });
+          console.log({ username, password, hashed_password });
           const payload = {
             username: username,
             password: hashed_password,
